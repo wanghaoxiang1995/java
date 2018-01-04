2449a5d74e7ade52502fbde918828d9bfad1b76c
王浩翔 <whx139@qq.com>
2018-01-04 17:43:30 +0800

    杂务 财务统计sql

--- src/com/test/mess/SqlGenerate.java
+++ src/com/test/mess/SqlGenerate.java
@@ -9,8 +9,8 @@ package com.test.mess;
  */
 public class SqlGenerate {
     
-    static String fromTime = "'2017-12-31'";
-    static String toTime = "'2018-01-01'";
+    static String fromTime = "'2017-12-01'";
+    static String toTime = "'2017-12-02'";
     
     
     public static void main(String[] args) {
@@ -29,14 +29,14 @@ public class SqlGenerate {
                                      + "UNION \n"
                                      + "SELECT account_id FROM pay_cash WHERE cash_status = 'cashed' and pay_cash_time >= %s and pay_cash_time <= %s\n"
                                      + "UNION\n"
-                                     + "SELECT from_account_id account_id FROM pay_trade WHERE create_time >= %s and create_time <=%s  and type = 2 \n"
+                                     + "SELECT from_account_id account_id FROM pay_trade WHERE create_time >= %s and create_time <=%s and debit_type = 2 and type = 2 \n"
                                      + "UNION\n"
-                                     + "SELECT to_account_id account_id FROM pay_trade WHERE create_time >= %s and create_time <=%s  and type = 3\n"
+                                     + "SELECT to_account_id account_id FROM pay_trade WHERE create_time >= %s and create_time <=%s and debit_type = 2 and type = 3\n"
                                      + ") a \n"
                                      + "LEFT JOIN(SELECT account_id,sum(price) price FROM pay_recharge b  WHERE create_time >= %s and create_time <=%s GROUP BY account_id   ) b on a.account_id = b.account_id\n"
                                      + "LEFT JOIN (SELECT account_id,sum(cash_money) cash_money FROM pay_cash c  WHERE c.cash_status = 'cashed' and c.pay_cash_time >= %s and c.pay_cash_time <= %s GROUP BY account_id) c on a.account_id = c.account_id \n"
-                                     + "LEFT JOIN (SELECT from_account_id account_id, sum(price) price FROM pay_trade d  WHERE d.create_time >= %s and d.create_time <=%s  and d.type = 2 GROUP BY from_account_id ) d on a.account_id = d.account_id\n"
-                                     + "LEFT JOIN (SELECT to_account_id account_id, sum(price) price FROM pay_trade e WHERE e.create_time >= %s and e.create_time <=%s  and e.type = 3 GROUP BY to_account_id) e on a.account_id = e.account_id \n"
+                                     + "LEFT JOIN (SELECT from_account_id account_id, sum(price) price FROM pay_trade d  WHERE d.create_time >= %s and d.create_time <=%s and debit_type = 2 and d.type = 2 GROUP BY from_account_id ) d on a.account_id = d.account_id\n"
+                                     + "LEFT JOIN (SELECT to_account_id account_id, sum(price) price FROM pay_trade e WHERE e.create_time >= %s and e.create_time <=%s and debit_type = 2 and e.type = 3 GROUP BY to_account_id) e on a.account_id = e.account_id \n"
                                      + "LEFT JOIN (SELECT account_id ,substring_index(group_concat(f.to_money * 0.01 order by f.create_time desc),\",\",1) as balance  FROM pay_xzb_record f WHERE   f.create_time >= %s and f.create_time <= %s  GROUP BY account_id) f on a.account_id = f.account_id \n"
                                      + "ORDER BY a.account_id asc;"
                 ,fromTime,
@@ -58,9 +58,9 @@ public class SqlGenerate {
                                      + "UNION \n"
                                      + "SELECT account_id FROM pay_cash WHERE cash_status = 'cashed' and pay_cash_time >= %s and pay_cash_time <= %s\n"
                                      + "UNION\n"
-                                     + "SELECT from_account_id account_id FROM pay_trade WHERE create_time >= %s and create_time <=%s  and type = 2 \n"
+                                     + "SELECT from_account_id account_id FROM pay_trade WHERE create_time >= %s and create_time <=%s and debit_type = 2 and type = 2 \n"
                                      + "UNION\n"
-                                     + "SELECT to_account_id account_id FROM pay_trade WHERE create_time >= %s and create_time <=%s  and type = 3\n"
+                                     + "SELECT to_account_id account_id FROM pay_trade WHERE create_time >= %s and create_time <=%s and debit_type = 2 and type = 3\n"
                                      + ") a , pay_xzb_record f WHERE a.account_id = f.account_id and f.create_time >= %s and f.create_time <= %s \n"
                                      + "GROUP BY a.account_id ORDER BY a.account_id asc;"
                 ,fromTime,toTime
@@ -70,4 +70,5 @@ public class SqlGenerate {
                 ,fromTime,toTime
         );
     }
+    
 }
