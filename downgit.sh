2b0eac175fe0c996d5d3e15ead83c10ae49a70bb
王浩翔 <whx139@qq.com>
2018-01-10 10:42:32 +0800

    项目下载脚本

---
+++ downgit.sh
@@ -0,0 +1,27 @@
+mkdir work
+cd work
+mkdir shigu
+cd shigu
+mkdir main-web cron pay open-search tb-site admin-site daifa8 xzmybatis productai zzixun img-search-old 
+mkdir main-web/main-pom
+git clone https://gitee.com/shigu_xz/main-pom.git main-web/main-pom
+mkdir cron/main4-cron
+git clone https://gitee.com/shigu_xz/main4-cron.git cron/main4-cron
+mkdir pay/pay_site
+git clone https://gitee.com/shigu_xz/pay_site.git pay/pay_site
+mkdir open-search/aliyun-open-search-sdk
+git clone https://gitee.com/shigu_xz/aliyun-open-search-sdk.git open-search/aliyun-open-search-sdk
+mkdir tb-site/tb_site_real
+git clone https://gitee.com/shigu_xz/tb_site_real.git tb-site/tb_site_real
+mkdir admin-site/sites_admin
+git clone https://gitee.com/shigu_xz/sites_admin.git admin-site/sites_admin
+mkdir daifa8/daifa_site8-0
+git clone https://gitee.com/shigu_xz/daifa_site8-0.git daifa8/daifa_site8-0
+mkdir xzmybatis/xinzuo-mybatis
+git clone https://gitee.com/shigu_xz/xinzuo-mybatis.git xzmybatis/xinzuo-mybatis
+mkdir productai/productai-java-sdk
+git clone https://gitee.com/shigu_xz/productai-java-sdk.git productai/productai-java-sdk
+mkdir zzixun/ztrade
+git clone https://gitee.com/shigu_xz/ztrade.git zzixun/ztrade
+mkdir img-search-old/img-search-sdk
+git clone https://gitee.com/shigu_xz/img-search-sdk.git img-search-old/img-search-sdk
